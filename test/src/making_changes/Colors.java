2a4b66a65aca3864bfa58e39522b18214eb1ee55
Michael Onishchenko <onishchenkomike@yahoo.com>
2023-09-28 23:24:27 -0700

    Added Color class

---
+++ test/src/making_changes/Colors.java
@@ -0,0 +1,12 @@
+package making_changes;
+
+public class Colors
+{
+    private String pink;
+    private String yellow;
+
+    public Colors(String pink, String yellow) {
+        this.pink = pink;
+        this.yellow = yellow;
+    }
+}
